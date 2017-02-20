e4352c41da6ca144992ff8e05e5208433befc5c5
Chuck Oguagha <coguagha1@gmail.com>
2017-02-20 13:25:01 -0500

    First draft

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $("#backwards_word_converter").click(function(){
+    var backwards_word = "";
+    var original_word = document.getElementById('input_word').value;
+    var i = 0;
+    while (i < original_word.length){
+      backwards_word = original_word[i] + backwards_word;
+      i++
+    }
+    document.getElementById('backwards').innerHTML = backwards_word
+  });
+
+});
