a34c7711c6ad399b8ddb43ca2faab712035a3159
Chuck Oguagha <coguagha1@gmail.com>
2017-03-01 11:32:01 -0500

    Fully functioning game

--- index.js
+++ index.js
@@ -42,12 +42,24 @@ $(document).ready(function() {
     if (answer.split("").reverse().join("") === words[i]) {
       document.getElementById('backwardsWord').innerHTML = words[i += 1];
       document.getElementById('points').innerHTML = points += 5;
-    }
-    if (answer.split("").reverse().join("") === words[i]) {
-      document.getElementById('correctOrWrong').innerHTML = "Correct"
+      document.getElementById('correctOrWrong').style.color = "green";
+      document.getElementById('correctOrWrong').innerHTML = "Correct";
+      //document.getElementById('correctOrWrong').innerHTML.style.color = "green"
     }else{
-      document.getElementById('correctOrWrong').innerHTML = "Wrong"
+      document.getElementById('correctOrWrong').style.color = "red";
+      document.getElementById('correctOrWrong').innerHTML = "Wrong";
+      document.getElementById('points').innerHTML = points -= 3;
     }
+
+    if (points < 0) {
+      alert("You lost");
+    }
+
+    // if (answer.split("").reverse().join("") === words[i]) {
+    //   document.getElementById('correctOrWrong').innerHTML = "Correct"
+    // }else{
+    //   document.getElementById('correctOrWrong').innerHTML = "Wrong"
+    // }
     
     // for (i = 0; i < words.length; i++) {
     //   word = words[i];
