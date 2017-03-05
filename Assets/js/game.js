67106b2d9899b9e7b11751531dfa1faa600b3962
Bill <bathe3rd@gmail.com>
2017-03-05 00:49:21 -0500

    Did some organizing of files

---
+++ Assets/js/game.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+ // Game JavaScript
+  var words = ['jay', 'hay', 'day'];
+  // var words1 = ['dfghjk', 'dfghjk'];
+  var i = 0;
+  var points = 0;
+  
+  function wordGenerator(){
+
+    var answer = document.getElementById('input_word').value
+    document.getElementById('backwardsWord').innerHTML = words[i];
+    $('#points').html(points);
+
+  }
+
+   $('#gameSubmit').click(function(){
+    var answer = document.getElementById('input_word').value;
+
+    if (answer.split("").reverse().join("") === words[i]) {
+      document.getElementById('backwardsWord').innerHTML = words[i += 1];
+      document.getElementById('points').innerHTML = points += 5;
+      document.getElementById('correctOrWrong').style.color = "green";
+      document.getElementById('correctOrWrong').innerHTML = "Correct";
+    }else{
+      document.getElementById('correctOrWrong').style.color = "red";
+      document.getElementById('correctOrWrong').innerHTML = "Wrong";
+      document.getElementById('points').innerHTML = points -= 3;
+    }
+
+    if (points < 0) {
+      alert("You lost");
+      document.getElementById('points').innerHTML = points = 0;
+    }
+
+    // if (answer.split("").reverse().join("") === words[i]) {
+    //   document.getElementById('correctOrWrong').innerHTML = "Correct"
+    // }else{
+    //   document.getElementById('correctOrWrong').innerHTML = "Wrong"
+    // }
+    
+    // for (i = 0; i < words.length; i++) {
+    //   word = words[i];
+    //    document.getElementById('backwardsWord').innerHTML = word.split("").reverse().join("");
+    // } 
+
+  //   //var j = document.getElementById("backwards").innerHTML;
+  //   document.getElementById("backwards").innerHTML = original_word;
+  //   k += 180;
+  //   document.getElementById("backwards").style.transform = "rotatey(" + k + "deg)";
+  //   document.getElementById("backwards").style.transitionDuration = "0.5s";
+
+  // $('#flip').click(function(){
+  //    var backwardsWord = "";
+  //    var flippedWord = document.getElementById('backwards').innerHTML;
+  //    var i = 0;
+  //    while (i < flippedWord.length){
+  //      backwardsWord = flippedWord[i] + backwardsWord;
+  //      i++
+  //      console.log(backwardsWord)
+  //    }
+  //    document.getElementById('backwards').innerHTML = backwardsWord;
+  //    $("#backwardsWords").append(backwardsWord + " ");
+    })
+  //  function wordGenerator1(){
+
+  //   var answer = document.getElementById('input_word').value
+  //   document.getElementById('backwardsWord1').innerHTML = words1[i];
+  //   $('#points').html(points);
+
+  // }
+  wordGenerator();
+  // wordGenerator1();
+});
+
